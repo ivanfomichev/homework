740062f5d75958a69046e42025884270fd3748ff
ivanfomichev <65482969+ivanfomichev@users.noreply.github.com>
2020-05-31 12:24:31 +0300

    Тестовые задачи.
    С _2 итоговое, без - первый неправильный вариант.

---
+++ first 2.py
@@ -0,0 +1,114 @@
+class NinjaTurtle:
+    name = ''
+    band_color = ""
+    weapon = ""
+    # абстрактный метод, который переопределяется в дочернем классе
+    def say(self):
+        raise NotImplementedError("Subclass must implement abstract method") 
+
+
+class Leonardo(NinjaTurtle):
+    name = 'Leonardo'
+    band_color = "blue"
+    weapon = "katana"
+    def say(self):
+        return "uhuuu!"
+
+
+class Michelangelo(NinjaTurtle):
+    name = 'Michelangelo'
+    band_color = "orange"
+    weapon = "nunchaku"
+    def say(self):
+        return "come on!"
+
+
+class Donatello(NinjaTurtle):
+    name = 'Donatello'
+    band_color = "purple"
+    weapon = "staff"
+    def say(self):
+        return "let's go!"
+
+class Raphael(NinjaTurtle):
+    name = 'Raphael'
+    band_color = "red"
+    weapon = "sai"
+    def say(self):
+        return "got it!"
+  
+
+class AbstractFactory:
+    def get_ninja(self, ninja_type):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+
+class NinjaFactory(AbstractFactory):
+    def get_ninja(self, ninja_type):
+        if ninja_type == 'Leonardo':
+            return Leonardo()
+        elif ninja_type == 'Michelangelo':
+            return Michelangelo()
+        elif ninja_type == 'Donatello':
+            return Donatello()
+        elif ninja_type == 'Raphael':
+            return Raphael()
+        else:
+            print('wrong type')
+
+factory = NinjaFactory()
+Leonardo = factory.get_ninja('Leonardo')
+Michelangelo = factory.get_ninja('Michelangelo')
+Donatello = factory.get_ninja('Donatello')
+Raphael = factory.get_ninja('Raphael')
+
+Leonardo.say()
+Michelangelo.say()
+Donatello.say()
+Raphael.say()
+
+print('Leonardo band color is', Leonardo.band_color, 'and weapon is', Leonardo.weapon)
+print('Michelangelo band color is', Michelangelo.band_color, 'and weapon is', Michelangelo.weapon)
+print('Donatello band color is', Donatello.band_color, 'and weapon is', Donatello.weapon)
+print('Raphael band color is', Raphael.band_color, 'and weapon is', Raphael.weapon)
+
+names = ['Leonardo', 'Michelangelo', 'Donatello', 'Raphael']
+ninjas = [Leonardo, Michelangelo, Donatello, Raphael]
+
+class Splinter:
+    students = {}
+    def __init__(self, names, ninjas):
+        self.names = names
+        self.ninjas = ninjas
+        Splinter.students = dict(zip(self.names, self.ninjas))
+
+    @staticmethod  
+    def get_student_band_color(student_name):
+        try:
+            student = Splinter.students.get(student_name,'def_wrong name')
+            return student.band_color
+        except:
+            print('wrong_name')
+        
+        
+    @staticmethod  
+    def get_student_weapon(student_name):
+        try:
+            student = Splinter.students.get(student_name)
+            if student is None:
+                return ('wrong_name_exception')
+            else:
+                return student.weapon
+        except:
+            print('wrong_name')
+
+
+master = Splinter(names, ninjas)
+print(master.get_student_band_color(1))
+print(master.get_student_weapon(2))
+print(master.get_student_band_color('Michelangelo'))
+print(master.get_student_weapon('Michelangelo'))
+print(master.get_student_band_color('Donatello'))
+print(master.get_student_weapon('Donatello'))
+print(master.get_student_band_color('Raphael'))
+print(master.get_student_weapon('Raphael'))
---
+++ second 2.py
@@ -0,0 +1,91 @@
+
+class Fighter:
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, name):
+        if type(name) is str:
+            self.__name = name
+        else:
+            raise Exception ('Error: not str type')
+    @property
+    def defence(self):
+        return self.__defence
+    @defence.setter
+    def defence(self, defence):
+        if type(defence) is int:
+            self.__defence = defence
+        else:
+            raise Exception ('Error: not int type')
+    @property
+    def strenth(self):
+        return self.__strenth
+    @strenth.setter
+    def strenth(self, strenth):
+        if type(strenth) is int:
+            self.__strenth = strenth
+        else:
+            raise Exception ('Error: not int type')
+    def fight(self, other):
+        if other.__strenth > self.__defence:
+            if self.__strenth > other.__defence:
+                print('ничья') 
+            else:
+                print("победитель", other.__name)
+        else:
+            if self.__strenth > other.__defence:
+                print("победитель", self.__name)
+            else:
+                print('ничья') 
+
+
+class Fighter_Builder:
+    def __init__(self):
+        self.fighter = None
+    def create_new_fighter(self,name,defence,strenth):
+        self.fighter = Fighter()
+        self.fighter.name = name
+        self.fighter.defence = defence
+        self.fighter.strenth = strenth
+        return self.fighter
+
+
+builder = Fighter_Builder()
+A = builder.create_new_fighter('One',4,5)
+B = builder.create_new_fighter('Two',3,7)
+C = builder.create_new_fighter('Three',2,4)
+D = builder.create_new_fighter('Four',20,4)
+E = builder.create_new_fighter('Five',20,41)
+
+
+class Championship:
+    @staticmethod
+    def championship(list):
+        for i, f1 in enumerate(list):
+            if i + 1 != len(list):
+                for j, f2 in enumerate(list):
+                   if j > i:
+                       f1.fight(f2)
+                           
+
+                    
+competitors = [A, B, C, D, E]              
+fighting = Championship()
+fighting.championship(competitors)
+print('\ncheck\n')
+A.fight(B)
+A.fight(C)
+A.fight(D)
+A.fight(E)
+B.fight(C)
+B.fight(D)
+B.fight(E)
+C.fight(D)
+C.fight(E)
+D.fight(E)
+
+
+
+
+ 
